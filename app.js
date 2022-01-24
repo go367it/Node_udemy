a8cc4a81254e576d378958a68f10db16b2ccb033
go367it <satyamkumar3001@gmail.com>
2022-01-24 22:07:46 +0530

    response html

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const http = require('http');
 
 const server = http.createServer((req, res)=>{
     console.log(req)
-    
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My first Page</title></head>')
+    res.write('</html>');
+    res.end();
 })
 
 server.listen(3000);
